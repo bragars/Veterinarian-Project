2fbb87797598895f3504f075019ee23198b75bbf
bragars <bragareispedro@gmail.com>
2019-11-18 16:43:17 -0300

    test commit

---
+++ tmp/Como_Funciona_Relationship_SqlAlchemy/one_to_N.py
@@ -0,0 +1,19 @@
+from flask import flask
+from flask_sqlalchemy import SQLALchemy
+
+app.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'swlite:///relationships.db
+
+db.SQLAlchemy(app)
+
+class Person(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+	pets = db.relationship('Pet', backref='owner')
+
+class Pet(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(20))
+	owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+
