ed00a0c04f9320dd266aec2d94fbc8f1bc2aa081
bragars <bragareispedro@gmail.com>
2019-12-02 19:43:25 -0300

    alterações no site, packages, modelagem

--- src/model/Animal.py
+++
@@ -1,6 +0,0 @@
-class Animal:
-    def __init__(self, especie, linhagem, genero, data_nascimento):
-        self.especie = especie
-        self.linhagem = linhagem
-        self.genero = genero
-        self.data_nascimento = data_nascimento
